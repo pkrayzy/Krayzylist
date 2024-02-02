e98efbcbc9c62d0061ff5ffcf9adebd1ea5f9e30
pkrayzy <pkrzystek@outlook.com>
2024-02-02 13:29:52 -0500

    update

--- NewsRedirector.js
+++ NewsRedirector.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         NewsRedirector
 // @namespace    http://tampermonkey.net/
-// @downloadURL  https://raw.githubusercontent.com/pkrayzy/Krayzylist/main/NewsRedirector
-// @updateURL    https://raw.githubusercontent.com/pkrayzy/Krayzylist/main/NewsRedirector
+// @downloadURL  https://raw.githubusercontent.com/pkrayzy/Krayzylist/main/NewsRedirector.js
+// @updateURL    https://raw.githubusercontent.com/pkrayzy/Krayzylist/main/NewsRedirector.js
 // @version      0.1
 // @description  Convert Weblinks into Google News Redirects
 // @author       You
