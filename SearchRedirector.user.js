339c85bbb97d6cab0c2b5483c5d2384b5c321da6
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2024-10-31 12:02:43 -0400

    Update SearchRedirector.user.js

--- SearchRedirector.user.js
+++ SearchRedirector.user.js
@@ -4,19 +4,17 @@
 // @run-at request
 // ==/UserScript==
 
-  {
-    "id": 1,
-    "priority": 1,
-    "action": {
-      "type": "redirect",
-      "redirect": {
-        "regexSubstitution": "https://search.brave.com/search?q=\\1"
-      }
+{
+    id: 1,
+    priority: 1,
+    action: {
+        type: 'redirect',
+        redirect: {
+            url: 'https://search.brave.com/search?q=$1'
+        }
     },
-    "condition": {
-      "regexFilter": "https://(www\\.)?duckduckgo\\.com/(.*)",
-      "resourceTypes": [
-        "main_frame"
-      ]
+    condition: {
+        urlFilter: 'https://duckduckgo.com/?q=*',
+        resourceTypes: ['main_frame']
     }
-  }
+}
