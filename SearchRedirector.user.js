e1a96b72f3093e0e51bc7ee2ea11014aa8a6281a
pkrayzy <peter.krzystek@outlook.com>
2025-01-05 11:45:09 -0500

    bing

--- SearchRedirector.user.js
+++ SearchRedirector.user.js
@@ -1,23 +1,28 @@
 // ==UserScript==
 // @name         Redirect Bing to Brave
-// @namespace    http://tampermonkey.net/
-// @version      1.0
 // @description  Redirects Bing searches to Brave Search
-// @author       Your Name
-// @match        *://www.bing.com/search*
-// @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    // Get the search terms from the URL
-    const params = new URLSearchParams(window.location.search);
-    const searchTerms = params.get('q');
-
-    // If search terms are found, redirect to Brave Search
-    if (searchTerms) {
-        const redirectUrl = `https://search.brave.com/search?q=${encodeURIComponent(searchTerms)}`;
-        window.location.replace(redirectUrl);
+[{
+  "id": 1,
+  "priority": 1,
+  "action": {
+    "type": "redirect",
+    "redirect": {
+      "transform": {
+        "host": "search.brave.com",
+        "path": "/search",
+        "queryTransform": {
+          "addOrReplaceParams": [{
+            "key": "q",
+            "value": "{searchTerms}"
+          }]
+        }
+      }
     }
-})();
+  },
+  "condition": {
+    "urlFilter": "*://www.bing.com/search*",
+    "resourceTypes": ["main_frame"]
+  }
+}]
