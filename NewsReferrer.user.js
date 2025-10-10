202a7ea613d24289bc94222ca03c2fb562ff17b9
pkrayzy <pkrayzy@users.noreply.github.com>
2025-10-10 09:31:21 -0400

    update referrer

--- NewsReferrer.user.js
+++ NewsReferrer.user.js
@@ -14,12 +14,30 @@
                 {
                     "header": "Referer",
                     "operation": "set",
-                    "value": "https://www.google.com/"
+                    "value": "https://www.duckduckgo.com/"
                 }
             ]
         },
         "condition": {
-            "urlFilter": "||ft.com/content/*",
+            "urlFilter": "||ft.com/*",
+            "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "xmlhttprequest", "ping", "media", "websocket", "other"]
+        }
+    },
+    {
+        "id": 202,
+        "priority": 1,
+        "action": {
+            "type": "modifyHeaders",
+            "requestHeaders": [
+                {
+                    "header": "Referer",
+                    "operation": "set",
+                    "value": "https://www.duckduckgo.com/"
+                }
+            ]
+        },
+        "condition": {
+            "urlFilter": "||nytimes.com/*",
             "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "xmlhttprequest", "ping", "media", "websocket", "other"]
         }
     }
