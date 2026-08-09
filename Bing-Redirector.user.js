7b4dd84c432c3c5d96cce1a36eaf64463a3b3194
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2026-08-08 23:17:42 -0400

    Rename script and update redirect URL pattern

--- Bing-Redirector.user.js
+++ Bing-Redirector.user.js
@@ -1,22 +1,21 @@
 // ==UserScript==
-// @name        DDG-Redirector
+// @name        Bing-Redirector
 // @run-at request
 // ==/UserScript==
 [
+  
   {
-    "id": 501,
+    "id": 601,
     "priority": 1,
     "action": {
       "type": "redirect",
       "redirect": {
-        "regexSubstitution": "https://search.brave.com/?q=+\\1"
+        "regexSubstitution": "https://search.brave.com/search?q=\\1"
       }
     },
     "condition": {
-      "regexFilter": "^https://bing.com/\\?(?:.*&)?q=([^&]+)(?:&.*)?$",
-      "resourceTypes": [
-        "main_frame"
-      ]
+      "regexFilter": "https?://(?:www\\.)?bing\\.com/search\\?.*q=([^&]+)",
+      "resourceTypes": ["main_frame"]
     }
   }
 ]
