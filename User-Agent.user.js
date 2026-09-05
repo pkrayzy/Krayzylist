b3aa1c3611b9f8a31df4744a5193a17342b296dc
pkrayzy <pkrayzy@users.noreply.github.com>
2026-09-05 12:02:19 -0400

    fix: update user-agent for iPhone and remove unused entry for YouTube

--- User-Agent.user.js
+++ User-Agent.user.js
@@ -38,37 +38,12 @@
         {
           "header": "user-agent",
           "operation": "set",
-          "value": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
+          "value": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.4 Mobile/15E148 Safari/604.1"
         }
       ]
     },
     "condition": {
       "urlFilter": "||x.com",
-      "resourceTypes": [
-        "main_frame",
-        "sub_frame",
-        "xmlhttprequest",
-        "script",
-        "image",
-        "stylesheet"
-      ]
-    }
-  },
-  {
-    "id": 303,
-    "priority": 1,
-    "action": {
-      "type": "modifyHeaders",
-      "requestHeaders": [
-        {
-          "header": "user-agent",
-          "operation": "set",
-          "value": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
-        }
-      ]
-    },
-    "condition": {
-      "urlFilter": "||youtube.com",
       "resourceTypes": [
         "main_frame",
         "sub_frame",
