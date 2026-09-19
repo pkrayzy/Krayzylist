08c4e8648647f262c7fdf145a437f54d6024d0f3
pkrayzy <pkrayzy@users.noreply.github.com>
2026-09-19 17:57:49 -0400

    fix: update system font stack to use simplified font list

--- Fonts.user.js
+++ Fonts.user.js
@@ -13,7 +13,7 @@
 (function () {
     'use strict';
 
-    const SYSTEM_FONT_STACK = "-apple-system, BlinkMacSystemFont, 'SF Pro Display', 'Arial', sans-serif";
+    const SYSTEM_FONT_STACK = "'SF Pro', 'Arial', sans-serif";
     const TARGET_FONTS = ['Google Sans', 'Google Sans Flex', 'Roboto', 'Segoe UI', 'Open Sans', 'Noto Sans','TwitterChirp'];
 
     function shouldReplaceFont(fontFamily) {
