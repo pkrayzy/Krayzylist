b82da65e92abd669c30726701d835215912211b8
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2026-09-19 23:36:15 -0400

    Simplify SYSTEM_FONT_STACK to use SF Pro Display

--- Fonts.user.js
+++ Fonts.user.js
@@ -36,7 +36,7 @@
     ];
 
     // The local font stack we want the browser to use when the target fonts are requested
-    const SYSTEM_FONT_STACK = "local('SF Pro'), local('SF Pro Display'), local('SF Pro Text'), sans-serif";
+    const SYSTEM_FONT_STACK = "local('SF Pro Display'), sans-serif";
 
     const cssOverrides = TARGET_FONTS.map(font => `
         @font-face {
