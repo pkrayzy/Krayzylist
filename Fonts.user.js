3dc83f6a2e1d4e785a888d69ab199cc643da2947
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2026-09-19 23:40:27 -0400

    Update SYSTEM_FONT_STACK to exclude 'sans-serif'
    Remove 'sans-serif' from the local font stack.

--- Fonts.user.js
+++ Fonts.user.js
@@ -36,7 +36,7 @@
     ];
 
     // The local font stack we want the browser to use when the target fonts are requested
-    const SYSTEM_FONT_STACK = "local('SF Pro Display'), sans-serif";
+    const SYSTEM_FONT_STACK = "local('SF Pro Display')";
 
     const cssOverrides = TARGET_FONTS.map(font => `
         @font-face {
