0eb003ba724f3d0c8b68bb0b719869678d6da215
pkrayzy <75462176+pkrayzy@users.noreply.github.com>
2026-09-19 23:24:36 -0400

    Update Fonts.user.js

--- Fonts.user.js
+++ Fonts.user.js
@@ -37,7 +37,7 @@
 
     // The local font stack we want the browser to use when the target fonts are requested
     // const SYSTEM_FONT_STACK = "local('SF Pro'), local('-apple-system'), local('Arial'), sans-serif";
-    const SYSTEM_FONT_STACK = "'SF Pro Text', 'Arial', sans-serif";
+    const SYSTEM_FONT_STACK = "'SF Pro', 'Arial', -apple-system, sans-serif";
 
     const cssOverrides = TARGET_FONTS.map(font => `
         @font-face {
